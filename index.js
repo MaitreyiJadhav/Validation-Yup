2793aa3f1958d8f2c2b8c480ceadd8dc970249f4
MaitreyiJadhav <maitreyijadhav95@gmail.com>
2019-03-20 21:28:55 -0700

    validation

---
+++ index.js
@@ -0,0 +1,31 @@
+const { send, json } = require('micro')
+
+const yup = require('yup');
+
+const myStructure = yup.object().shape({
+    name: yup.string().required(),
+    age: yup
+      .number()
+      .positive()
+      .required(),
+      email: yup.string().email().required()
+
+  });
+
+module.exports = async (req, res) => {
+    const data = await json(req)
+    myStructure.isValid(data)
+    .then(function(valid) {
+      
+      if(valid){
+        send(res, 200, data)
+
+      }
+      else{
+          send(res, 400, "Your data does not validate")
+      }
+    }).catch(function(){
+        send(res, 400, data)
+    })
+}
+
